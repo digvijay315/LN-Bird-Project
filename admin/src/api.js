c1c0c4fffbcbcfe237dc60b11e3f97748916e464
niketnarayan <narayanniket2@gmail.com>
2025-01-21 23:41:53 +0530

    change api of cpanel

--- admin/src/api.js
+++ admin/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 const instance=axios.create({
    
-         baseURL:'https://ln-bird-project-7.onrender.com/'
+        //  baseURL:'https://ln-bird-project-7.onrender.com/'
+        baseURL:' http://onlinerealestatecrm.in/'
         // baseURL:'http://localhost:5000/'
+       
 })
 export default instance;
