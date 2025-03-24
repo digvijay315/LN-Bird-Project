2ceef466913913e3eeb72383599838c3d82963c1
niketnarayan <narayanniket2@gmail.com>
2025-03-24 17:08:30 +0530

    url changed

--- bekiona-backend/index.js
+++ bekiona-backend/index.js
@@ -16,10 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/images', express.static('images'));
 // app.use('/images', express.static(path.join(__dirname, 'images')));
 // app.use(cors())
+app.use(cors({ origin: 'https://ln-bird-project-6m7b.vercel.app/' }));
 app.use(cors({
-    origin: "https://bekiona.com",  // Allow your Vercel domain
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
+    origin: 'https://ln-bird-project-6m7b.vercel.app', // Vercel frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // List of allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // List of allowed headers
   }));
 connect();
 app.get('/',(req,res)=>
