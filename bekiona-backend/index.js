98e4b464264ec9cb3088b94f4420377af6b69503
niketnarayan <narayanniket2@gmail.com>
2025-03-24 16:15:24 +0530

    url changed

--- bekiona-backend/index.js
+++ bekiona-backend/index.js
@@ -15,7 +15,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 // app.use('/images', express.static('images'));
 // app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: 'https://www.bekiona.com/' }));
+app.use(cors({
+    origin: 'https://www.bekiona.com', // Vercel frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // List of allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // List of allowed headers
+  }));
 connect();
 app.get('/',(req,res)=>
 {
