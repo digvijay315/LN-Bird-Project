3e24a38f467a40b9a30f49d50aab02bb41ffae93
niketnarayan <narayanniket2@gmail.com>
2025-04-03 18:28:33 +0530

    bekiona fixed

--- bekiona-frotend/src/Components/Header.js
+++ bekiona-frotend/src/Components/Header.js
@@ -284,7 +284,7 @@ console.log('Order Created:', order);
 
 // Step 2: Razorpay Checkout Options
 const options = {
-key: 'rzp_test_kh59VKLP3zCcop',
+key: 'rzp_live_YBXf8NJT3Al7Qc',
 amount: order.amount,
 currency: order.currency,
 name: 'Your Company Name',
