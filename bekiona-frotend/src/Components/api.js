721aa5b4964a662260f9a27b9482c4199616863a
niketnarayan <narayanniket2@gmail.com>
2025-05-14 13:06:55 +0530

    bug fixed

--- bekiona-frotend/src/Components/api.js
+++ bekiona-frotend/src/Components/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance=axios.create({
     
         //    baseURL:'https://ln-bird-project.onrender.com/'
-           baseURL:' http://51.21.180.27:5000/'
+           baseURL:'http://51.21.180.27:5000/'
         // baseURL:'https://onlinerealestatecrm.in/'
         // baseURL:'http://localhost:5000/'
        
