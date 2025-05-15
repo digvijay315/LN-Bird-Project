68b79c9c12a0249ed0e8148f726525725c32e62b
niketnarayan <narayanniket2@gmail.com>
2025-05-15 16:44:41 +0530

    hari ecommerce

---
+++ harri-backend/config/db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { secret } = require('./secret');
+
+mongoose.set('strictQuery', false);
+
+// it is local url 
+// const DB_URL = 'mongodb://127.0.0.1:27017/hamart';
+ const DB_URL="mongodb+srv://digvijay:digvijay@cluster0.yisihts.mongodb.net/hamart"
+// it is mongodb url
+const MONGO_URI = secret.db_url;
+
+const connectDB = async () => {
+  try { 
+    await mongoose.connect(MONGO_URI);
+    console.log('mongodb connection success!');
+  } catch (err) {
+    console.log('mongodb connection failed!', err.message);
+  }
+};
+
+module.exports = connectDB;
