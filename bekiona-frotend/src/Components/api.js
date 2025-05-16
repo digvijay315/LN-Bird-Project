16724060c1999f79d34a55d3bd672c3b2d8ad5de
niketnarayan <narayanniket2@gmail.com>
2025-05-16 16:59:22 +0530

    bekiona deployed

--- bekiona-frotend/src/Components/api.js
+++ bekiona-frotend/src/Components/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance=axios.create({
     
         //    baseURL:'https://ln-bird-project.onrender.com/'
-           baseURL:'http://13.60.193.45:5000/'
+           baseURL:'http://api.bekiona.com:5000/'
         // baseURL:'https://onlinerealestatecrm.in/'
         // baseURL:'http://localhost:5000/'
        
