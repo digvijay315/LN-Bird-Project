76cef66f12ee946285278f1cff5d09433b2f9dc7
niketnarayan <narayanniket2@gmail.com>
2025-05-16 17:19:55 +0530

    ssl done

--- bekiona-frotend/src/Components/api.js
+++ bekiona-frotend/src/Components/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance=axios.create({
     
         //    baseURL:'https://ln-bird-project.onrender.com/'
-           baseURL:'http://api.bekiona.com:5000/'
+           baseURL:'https://api.bekiona.com/'
         // baseURL:'https://onlinerealestatecrm.in/'
         // baseURL:'http://localhost:5000/'
        
