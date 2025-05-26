d6587940c27366140ba06b0adc380c72797b02ef
niketnarayan <narayanniket2@gmail.com>
2025-05-26 18:52:51 +0530

    url updated

--- crm backend/index.js
+++ crm backend/index.js
@@ -13,7 +13,9 @@ const app=express();
 // Increase request body size limit
 app.use(bodyParser.json({ limit: "50mb" })); // Increase limit for JSON payloads
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // Increase limit for form data
-app.use(express.json())
+// Allow up to 50MB of JSON data (adjust as needed)
+app.use(express.json({ limit: '50mb' }));
+
 // app.use('/images', express.static('images'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 // app.use(cors())
