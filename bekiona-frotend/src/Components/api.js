df1eb9a3057a2421a1f3a7fbfb5a50c1be98c4cb
niketnarayan <narayanniket2@gmail.com>
2025-06-06 16:44:33 +0530

    api updated

--- bekiona-frotend/src/Components/api.js
+++ bekiona-frotend/src/Components/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 const instance=axios.create({
     
         //    baseURL:'https://ln-bird-project.onrender.com/'
-           baseURL:'https://api.bekiona.com/'
+        //    baseURL:'https://api.bekiona.com/'
         // baseURL:'https://onlinerealestatecrm.in/'
-        // baseURL:'http://localhost:5000/'
+        baseURL:'http://localhost:5000/'
        
 })
 export default instance;
