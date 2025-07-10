02b11e848d6c3231e10d79e47ff6778f7018ce9e
niketnarayan <narayanniket2@gmail.com>
2025-07-10 10:40:31 +0530

    fav icon changed

--- lawyer frontend/src/components/socket.js
+++ lawyer frontend/src/components/socket.js
@@ -1,12 +1,12 @@
 // socket.js
 import { io } from 'socket.io-client';
 
-// const socket = io('http://localhost:5000', {
-//   autoConnect: false, // important: don't auto-connect immediately
-// });
-
-const socket = io('https://ln-bird-project.onrender.com', {
+const socket = io('http://localhost:5000', {
   autoConnect: false, // important: don't auto-connect immediately
 });
+
+// const socket = io('https://ln-bird-project.onrender.com', {
+//   autoConnect: false, // important: don't auto-connect immediately
+// });
 
 export default socket;
