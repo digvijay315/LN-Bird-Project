9505c0003a4a5c093659f8f804b7b0ff1165c6b2
digvijay315 <digvijaykumar.315@gmail.com>
2025-08-15 14:03:06 +0530

    tab changed

--- Bizaariocare-new-main/src/AppLayout/Header.jsx
+++ Bizaariocare-new-main/src/AppLayout/Header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
                 <li><a className="dropdown-item" href="/">Hindi</a></li>
               </ul>
             </div>
-            <a href="https://ln-bird-project-6sul-phqgl9zm1.vercel.app/signin" target='_blank' className="btn nav-btn-style">Login</a>
+            <a href="https://ln-bird-project-6sul-phqgl9zm1.vercel.app/signin"  className="btn nav-btn-style">Login</a>
             <a href="https://ln-bird-project-6sul-phqgl9zm1.vercel.app/register" className="btn nav-btn-style2">Sign Up</a>
           </div>
         </div>
